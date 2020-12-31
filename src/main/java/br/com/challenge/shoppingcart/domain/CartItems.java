d3d72f3ef1337675ff7e2d14b8babb19da4b358a
jaime <jaime.nascimento@mv.com.br>
2020-12-31 14:32:01 -0300

    CreatedEntitiesAndConfiguredDataBadeH2

---
+++ src/main/java/br/com/challenge/shoppingcart/domain/CartItems.java
@@ -0,0 +1,60 @@
+package br.com.challenge.shoppingcart.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItems extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Integer quantity;
+    private BigDecimal unitValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitValue() {
+        return unitValue;
+    }
+
+    public void setUnitValue(BigDecimal unitValue) {
+        this.unitValue = unitValue;
+    }
+}
---
+++ src/main/java/br/com/challenge/shoppingcart/dto/PromoCodeDTO.java
@@ -0,0 +1,33 @@
+package br.com.challenge.shoppingcart.dto;
+
+import java.math.BigDecimal;
+
+public class PromoCodeDTO {
+    private Long id;
+    private String code;
+    private BigDecimal discountPercentage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(BigDecimal discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+}
